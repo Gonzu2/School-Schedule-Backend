c0bcaaa6fe9fcbac4c907e5c749e65dc679c558c
gonzu2 <gonzuwu2@gmail.com>
2023-10-04 13:16:00 +0300

    a

--- src/db.js
+++ src/db.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-undef */
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const lessonsSchema = require("../db/lessons.module");
 const classesSchema = require("../db/classes.module");
+const User = require("../db/user.module");
 
 async function getLessons() {
   try {
@@ -24,22 +27,62 @@ async function getClasses() {
   }
 }
 
-const db = async (mongodb_url) => {
-  await mongoose
-    .connect(mongodb_url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("Connected to the Database.");
-    })
-    .catch((err) => console.error(err));
-
+async function insertUserIfNotExists(username, password) {
   try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      console.log("User already exists:", existingUser);
+      return existingUser;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      username: username,
+      password: hashedPassword,
+    });
+
+    await newUser.save(); // Save the new user to the database
+    console.log("User inserted successfully:", newUser);
+    return newUser;
+  } catch (error) {
+    console.error("Error inserting user:", error);
+    throw error;
+  }
+}
+
+async function getUser(username) {
+  try {
+    const user = await User.findOne({ username }).lean();
+    return user;
+  } catch (error) {
+    console.error("Error retrieving user data:", error);
+    throw error;
+  }
+}
+
+const db = async (mongodb_url) => {
+  try {
+    await mongoose
+      .connect(mongodb_url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Connected to the Database.");
+      })
+      .catch((err) => console.error(err));
+
+    const defaultUsername = "admin"; // Change this to the desired default username
+    const defaultPassword = "admin123"; // Change this to the desired default password
+    const defaultUser = await insertUserIfNotExists(
+      defaultUsername,
+      defaultPassword
+    );
+
     const lessons = await getLessons();
     const classes = await getClasses();
 
-    return { lessons, classes };
+    return { lessons, classes, getUser };
   } catch (error) {
     console.error("Error in the main function:", error);
     throw error;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./db");
 const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
 
 dotenv.config();
 
@@ -17,19 +18,39 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.use(express.json());
 const port = 5231;
 
 // Call the db function and handle the retrieved data
 db(mongodb_url)
   .then((data) => {
-    const { lessons, classes } = data; // Destructure the object to access lessons and classes
-    console.log(lessons);
+    const { lessons, classes, getUser } = data; // Destructure the object to access lessons and classes
 
     app.get("/getLessons", (req, res) => {
       res.send(lessons);
     });
     app.get("/getClasses", (req, res) => {
       res.json(classes);
+    });
+
+    app.post("/login", async (req, res) => {
+      console.log("Received login");
+      console.log(req.body);
+      const { username, password } = req.body;
+
+      try {
+        const user = await getUser(username);
+
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: "Login successful" });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
     });
   })
   .catch((error) => {
